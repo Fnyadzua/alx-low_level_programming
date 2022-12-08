b72ef9e1adaafe89b526b1a103cb6ff3062cfec6
Fnyadzua <nyadzuambodze@gmail.com>
2022-12-08 02:04:19 -0800

    task 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,35 +1,12 @@
 #include <stdio.h>
-
-
-
 /**
+ * main - print a string of characters
  *
- *   * main - Print a string of characters
- *
- *     *
- *
- *       * Return: always 0 (Success)
- *
- *         */
-
-
-
+ * Return: always 0 (success)
+ */
 int main(void)
-
-
-
 {
-
-		char my_string[] = "\"Programming is like building a multilingual puzzle";
-
-
-
-			puts(my_string);
-
-
-
-				return (0);
-
-
-
+	char my_string[] = "\"Programming is like building a multilingual puzzle";
+	puts(my_string);
+	return (0);
 }
