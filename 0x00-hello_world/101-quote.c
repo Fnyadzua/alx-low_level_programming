ac6b3e505087833d2ad9d4643e2f8e1342176f81
Fnyadzua <nyadzuambodze@gmail.com>
2022-12-08 11:14:40 -0800

    task 9

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,10 +9,9 @@
  */
  
 int main(void)
- 
- {
+{
 	 char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
+	 
  /*
   * write(STDOUT_FILENO, "hello world", strlen("hello world"));
   */
