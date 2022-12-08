9d294c48b59b8ee491fac713b851b53162a59b20
Fnyadzua <nyadzuambodze@gmail.com>
2022-12-08 02:43:14 -0800

    task 6

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,29 +1,12 @@
 #include <stdio.h>
-
-
-
 /**
+ * main - Print the exact string given
  *
- *   * main - Print the exact string given
- *
- *     *
- *
- *       * Return: Always 0 (success)
- *
- *         */
-
-
-
+ * Return: Always 0 (success)
+ */
 int main(void)
-
 {
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-
-
-
-			return (0);
-
-
-
+	return (0);
 }
