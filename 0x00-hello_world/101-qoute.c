df6495c6ecc84c4f72ab8405eac40f2c41dff944
Fnyadzua <nyadzuambodze@gmail.com>
2022-12-08 10:33:47 -0800

    task 9

---
+++ 0x00-hello_world/101-qoute.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <unstid.h>
+#include <string.h>
+
+/**
+ * main -print a string to the standard error (2)
+ *
+ * Return: return 1 the value of standard output
+ * /
+ 
+ int main(void)
+ {
+
+ char str[] = "and that piece of art is useful\" - Dora korpar, 2015-10-19\n";
+ 
+/*
+  * write(STDOUT_FILENO, "hello world", strlen("hello world"));
+  */
+
+write(2, str, strlen(str));
+
+return (1);
+}
