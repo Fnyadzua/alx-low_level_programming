8cfb8bad31fdc19f94a0ca06c5abfe01b6921d1d
Fnyadzua <nyadzuambodze@gmail.com>
2022-12-08 11:43:54 -0800

    task 9

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,8 +6,7 @@
  * main -print a string to the standard error (2)
  *
  * Return: return 1 the value of standard output
- */
- 
+ */ 
 int main(void)
 {
 	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
