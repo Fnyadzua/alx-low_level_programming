1ffe293348ea04e344ce196b00b03cbe273b55da
Fnyadzua <nyadzuambodze@gmail.com>
2022-12-09 14:36:45 -0800

    task 8

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main -main block
+ * using putchar followed by a new line
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a =48; a < 58; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
+
