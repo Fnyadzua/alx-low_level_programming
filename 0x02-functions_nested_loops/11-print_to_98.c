9e65cde9ac6fa891062c2e121bbd7bb5892bf97b
Fnyadzua <nyadzuambodze@gmail.com>
2022-12-14 10:57:42 -0800

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,29 +7,19 @@
  */
 void print_to_98(int n)
 {
-	int i = 0;
-
-	if (n > 98)
+	while (n > 98)
 	{
-		for (i = n; i >= 98; i--)
-		{
-			if (i != n)
-			{
-				printf(", ");
-			}
-			printf("%d, i);
-		}
+		printf("%d, ", n);
+		n--;
 	}
-	else
+	while (n < 98)
 	{
-		for (i = n; i <= 98; i++)
-		{
-			if (i != n)
-			{
-				printf(" , ");
-			}
-			printf("%d", i);
-		}
+		printf("%d, ", n);
+		n++;
 	}
-	putchar('\n');
+	if (n == 98)
+	{
+		print("%d", n);
+	}
+	printf("\n");
 }
