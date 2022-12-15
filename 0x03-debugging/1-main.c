5e8c7077a588ecc93f51286a5195c7cec6d89ac7
Fnyadzua <nyadzuambodze@gmail.com>
2022-12-15 03:50:39 -0800

    task 1

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,23 @@
+#include <stdio.>
+/**
+ * main - causes an infite loop
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	printf("infinite looop incoming :(\n");
+
+	i = 0;
+	/*
+	 * while (i < 10)
+	 *{
+	 * putchar(i);
+	 * }
+	 */
+	
+	printf("infinite loop avoided! \\o/\n");
+
+	return (0);
+}
