5989a5852d0b013f6c35fbc30101650176f21a38
Fnyadzua <nyadzuambodze@gmail.com>
2022-12-15 05:03:47 -0800

    task 102

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -11,11 +11,11 @@ int main(void)
 	unsigned long int bef = 1;
 	unsigned long int aft = 2;
 
-	printf("%lu", aft);
+	printf("%lu", bef);
 
 	for (i = 1; i < 50; i++)
 	{
-		printf(", %lu" , aft);
+		printf(", %lu", aft);
 		aft += bef;
 		bef = aft - bef;
 	}
