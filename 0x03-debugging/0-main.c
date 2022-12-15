3c2150731f8fbb8f94614150471bfd600d67b566
Fnyadzua <nyadzuambodze@gmail.com>
2022-12-15 03:37:05 -0800

    task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -7,15 +7,8 @@ int main(void)
 {
 	int i;
 
-	printf("infinite loop incoming :(\n")
-
-		i = 0;
-	/*
-	 * while (i < 10)
-	 * {
-	 * putchar(i);
-	 * }
-	 */
-	printf("infinite loop avoided! \\o/\n");
+	i = 0;
+	positive_or_negative(i);
+	
 	return (0);
 }
