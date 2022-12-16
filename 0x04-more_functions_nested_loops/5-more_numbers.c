286e7aec8c0cb5bbfa4c40b323754e691f8eda5c
Fnyadzua <nyadzuambodze@gmail.com>
2022-12-16 09:39:26 -0800

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -15,11 +15,12 @@ void more_numbers(void)
 			c = n;
 			if (n > 9)
 			{
-				_putchar('0' + c);
+				_putchar('1');
 				c = n % 10;
 			}
 			_putchar('0' + c);
 		}
 		_putchar('\n');
+		i++;
 	}
 }
