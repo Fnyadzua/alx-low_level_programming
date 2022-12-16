946e55f56dab3a4516d2f18c7be770f559c3b0d9
Fnyadzua <nyadzuambodze@gmail.com>
2022-12-16 10:29:19 -0800

    task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_diagnol - print diagnol line
+ * @n: length of line
+ * Return: void
+ */
+void print_diagnol(int n)
+{
+	int draw, space;
+
+	if (n > 0)
+	{
+		for (draw = 1; draw <= n; draw++)
+		{
+			for (space = 1; space < draw; space++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
