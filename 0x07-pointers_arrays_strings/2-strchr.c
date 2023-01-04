bc3c0a149a33f0ff68cfefe82756271f51c40753
Fnyadzua <nyadzuambodze@gmail.com>
2023-01-03 22:50:42 -0800

    task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strchr - locate character in string
+ * @s: string to check
+ * @c: character to find
+ * Return: Null or pointer if found
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; *s(s + i) != '\0'; i++)
+	{
+		if (*(s + i) == c)
+			break;
+	}
+	if (*(s + i) == c)
+		return (s + i);
+	else
+		return ('\0');
+}
