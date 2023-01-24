fa9bb2ac553e60232dee483e032c7b9a1d7d1c3f
Fnyadzua <nyadzuambodze@gmail.com>
2023-01-24 04:56:15 -0800

    task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+/**
+ * print_name - print the name
+ * @name: the name to be printed
+ * @f: function pointer
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}
