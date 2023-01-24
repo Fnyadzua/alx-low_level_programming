a0cd3f46aed33887bcb5db7bbd3ca9cabdf52e7a
Fnyadzua <nyadzuambodze@gmail.com>
2023-01-24 05:56:29 -0800

    task 1

--- 0x0F-function_pointers/1-main.c
+++ 0x0F-function_pointers/1-main.c
@@ -8,7 +8,17 @@
  */
 void print_elem(int elem)
 {
-	printf("%\n", elem);
+	printf("%d\n", elem);
+}
+/**
+ * print_elem_hex - prints an integer, in hexadecimal
+ * @elem: the integer to print
+ *
+ * Return: Nothing
+ */
+void print_elem_hex(int elem)
+{
+	printf("0x%x\n", elem);
 }
 /**
  * main - check the code
