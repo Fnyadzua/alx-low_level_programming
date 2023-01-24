71bf9069c5760b3e347b838eefc65fe27d974337
Fnyadzua <nyadzuambodze@gmail.com>
2023-01-24 05:47:39 -0800

    task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - loops through the array
+ * @array: the array to loop through
+ * @size: size of array
+ * @action: function to call on each else
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+	
+	if (array != NULL && size > 0 && action != NULL)
+	{
+		for (i = 0; i < size; i++)
+			action(array[i]);
+	}
+}
