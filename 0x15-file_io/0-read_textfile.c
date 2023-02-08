d2ad263010ef93de70fc97793e0b8f1ea7deb768
Fnyadzua <nyadzuambodze@gmail.com>
2023-02-08 05:43:02 -0800

    task 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * read_textfile - reads and prints the content of the file
+ * @filename: pointer to the file
+ * @letters: no of bytes
+ * Return: no of bytes printed or 0
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	char *buff;
+	ssize_t nread, nwrite;
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	buff = malloc(sizeof(char) * letters);
+
+	if (buff == Null)
+		return (0);
+
+	nread = read(fd, buff, letters);
+
+	if (nread == -1)
+		return (0);
+
+	close(fd);
+
+	free(buff);
+
+	return (nwrite);
+
+}
